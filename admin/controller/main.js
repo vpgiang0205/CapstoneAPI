ca0508187128c0feb1fb735c433c1af3a1cddff2
Your Name <you@example.com>
2023-06-07 06:55:47 +0700

    add the delete button

--- admin/controller/main.js
+++ admin/controller/main.js
@@ -15,6 +15,56 @@ function getEle(id) {
     return document.getElementById(id)
 }
 
+// Add
+function productAdd() {
+    var name = getEle('name').value;
+    var price = getEle('price').value;
+    var screen = getEle('screen').value;
+    var blackCamera = getEle('blackCamera').value;
+    var frontCamera = getEle('frontCamera').value;
+    var img = getEle('img').value;
+    var desc = getEle('desc').value;
+    var type = getEle('type').value;
+
+
+    var product = {
+        name,
+        price,
+        screen,
+        blackCamera,
+        frontCamera,
+        img,
+        desc,
+        type,
+    }
+
+    axios.post('https://64709e4f3de51400f724a111.mockapi.io/product')
+        .then((response) => {
+            console.log(response);
+            console.log("added")
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+}
+
+
+// Delete
+function productDelete(id) {
+    console.log(id)
+    axios.delete(`https://64709e4f3de51400f724a111.mockapi.io/product/${id}`)
+        .then((response) => {
+            alert(`deleted product with id: ` + id)
+            fetchData().then(() => {
+                renderProduct();
+            })
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+}
+
+
 // Render: 
 var renderProduct = () => {
     var contentHTML = "";
@@ -32,7 +82,7 @@ var renderProduct = () => {
         <td>
         <button class = "btn btn-danger" data-toggle="modal" data-target="#modelId" value ="${product.id}">Edit</button>
         
-        <button class = "btn btn-dark" value ="${product.id}">Delete</button>
+        <button class = "btn btn-dark" value ="" onclick="productDelete(${product.id})">Delete</button>
         </td>
         </tr>
         `
@@ -41,7 +91,7 @@ var renderProduct = () => {
     }
 }
 
-// Run the function:
+// Run this function to rerender the table:
 fetchData().then(() => {
     renderProduct();
 })
